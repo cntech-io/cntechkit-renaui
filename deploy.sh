022a19534fda034395c8647b503b9f7625bdc9e2
Yunus <yunus@cntech.io>
2023-09-06 21:13:36 +0300

    deploysh completed

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
-echo "building before publish..."
+echo ">>> building before publish..."
 yarn build
-echo "commiting before version..."
+echo ">>> committing before version..."
 echo "enter message:"
 read MESSAGE
 git add .
 git commit -m "$MESSAGE"
+echo ">>> patching version..."
 VERSION=$(npm version patch)
-echo $VERSION
+echo ">>> version is $VERSION"
+git push origin main
+git tag $VERSION
+git push origin $VERSION
+npm publish --access public
