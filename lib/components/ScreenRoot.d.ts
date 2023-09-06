e4c7bb58dcd262087b5526b66636e61928445dd2
Yunus <yunus@cntech.io>
2023-09-06 19:15:51 +0300

    ScreenRoot component development

--- lib/components/ScreenRoot.d.ts
+++ lib/components/ScreenRoot.d.ts
@@ -2,9 +2,12 @@ import React from "react";
 import { ViewStyle } from "react-native";
 export type ScreenRootProps = {
     children?: React.ReactNode;
-    style?: ViewStyle;
+    style?: ViewStyle | ViewStyle[];
+    safeAreaFlag?: boolean;
+    header?: React.JSX.Element;
 };
 export type ScreenRootState = {};
 export default class ScreenRoot extends React.Component<ScreenRootProps, ScreenRootState> {
+    renderHeader: () => React.JSX.Element | null;
     render(): React.JSX.Element;
 }
