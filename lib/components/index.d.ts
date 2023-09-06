9aed8456aaf1a5e23c7d114175f552e876f0d6ca
Yunus <yunus@cntech.io>
2023-09-07 02:05:31 +0300

    exported Header, Spacer, Icon,IconButton components

--- lib/components/index.d.ts
+++ lib/components/index.d.ts
@@ -1,4 +1,8 @@
 import Flex from "./Flex";
 import ScreenRoot from "./ScreenRoot";
 import Label from "./Label";
-export { ScreenRoot, Flex, Label };
+import Header from "./Header";
+import Spacer from "./Spacer";
+import Icon from "./Icon";
+import IconButton from "./IconButton";
+export { ScreenRoot, Flex, Label, Header, Spacer, Icon, IconButton };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,9 @@
 import Flex from "./Flex";
 import ScreenRoot from "./ScreenRoot";
 import Label from "./Label";
+import Header from "./Header";
+import Spacer from "./Spacer";
+import Icon from "./Icon";
+import IconButton from "./IconButton";
 
-export { ScreenRoot, Flex, Label };
+export { ScreenRoot, Flex, Label, Header, Spacer, Icon, IconButton };
