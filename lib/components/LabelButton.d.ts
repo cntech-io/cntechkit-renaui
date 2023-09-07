13711ec927f0527954e0f806482e486ddf7d70aa
Yunus <yunus@cntech.io>
2023-09-08 02:54:34 +0300

    LinkLabel new props added

--- lib/components/LabelButton.d.ts
+++ lib/components/LabelButton.d.ts
@@ -1,7 +1,13 @@
 import React from "react";
+import { ViewStyle } from "react-native";
 export declare type LinkLabelProps = {
     label: string;
     onPress: () => void;
+    style?: ViewStyle | ViewStyle[];
+    bold?: boolean;
+    noMargin?: boolean;
+    color?: string;
+    underlineColor?: string;
 };
 declare const LinkLabel: (props: LinkLabelProps) => React.JSX.Element;
 export default LinkLabel;
--- src/components/LabelButton.tsx
+++ src/components/LabelButton.tsx
@@ -1,18 +1,51 @@
 import React from "react";
-import { TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  TextStyle,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from "react-native";
 import Label from "./Label";
 
 export type LinkLabelProps = {
   label: string;
   onPress: () => void;
+  style?: ViewStyle | ViewStyle[];
+  bold?: boolean;
+  noMargin?: boolean;
+  color?: string;
+  underlineColor?: string;
 };
 
 const LinkLabel = (props: LinkLabelProps) => {
   return (
     <TouchableOpacity onPress={props.onPress}>
-      <Label>{props.label}</Label>
+      <Label
+        color={props.color}
+        noMargin={props.noMargin}
+        bold={props.bold}
+        style={[
+          styles.label,
+          props.underlineColor !== ""
+            ? {
+                borderBottomColor: props.underlineColor,
+              }
+            : {},
+          StyleSheet.flatten(props.style),
+        ]}
+      >
+        {props.label}
+      </Label>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  label: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+  },
+});
 
 export default LinkLabel;
